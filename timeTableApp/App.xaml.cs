e47b6fb831a04dcc65cb584b797a98d1d9c17a27
4uhar1k <vovas0712@gmail.com>
2024-10-22 15:33:31 +0200

    Добавьте файлы проекта.

---
+++ timeTableApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace timeTableApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ timeTableApp/Models/TimeTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timeTableApp.Models
+{
+    public class TimeTable
+    {
+        public Dictionary<int, Event> eventsList { get; set; }
+
+
+    }
+}
